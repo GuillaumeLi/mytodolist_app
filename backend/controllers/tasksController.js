86bdc2117ab57f9a6b543abbd0bc918a37b6a9d5
GuillaumeLi <81261803+GuillaumeLi@users.noreply.github.com>
2026-09-10 18:40:11 +0200

    Adding controllers for the request logic and response

---
+++ backend/controllers/tasksController.js
@@ -0,0 +1,120 @@
+const pool = require("../database");
+
+async function getTasks (req, res) {
+    try {
+        const result = await pool.query("SELECT * FROM tasks");
+        res.json(result.rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: "Error while querying tasks"});
+    }
+}
+
+async function createTask (req, res) {
+    // Validate the request body
+    if (req.body.title === undefined ||
+        typeof req.body.title !== "string" ||
+        req.body.title.trim() === "") {
+        return res.status(400).json({message: "Title must be defined and must be a non-empty string"});
+    }
+    if (req.body.description !== undefined &&
+        typeof req.body.description !== "string") {
+        return res.status(400).json({message: "Description must be a string"});
+    }
+    if (req.body.completed !== undefined &&
+        typeof req.body.completed !== "boolean") {
+        return res.status(400).json({message: "Completed must be a boolean"});
+    }
+
+    try {
+        const description = req.body.description !== null ? req.body.description.trim() : null;
+        const result = await pool.query(
+            `INSERT INTO tasks (title, description)
+            VALUES ($1, $2) 
+            RETURNING *`,
+            [req.body.title.trim(), description]);
+        const newTask = result.rows[0];
+        res.status(201).json(newTask);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: "Error while creating a new task"});
+    }
+}
+
+async function updateTask (req, res) {
+    const id = req.params.id;
+    const fields = [];
+    const values = [];
+
+    if (Object.keys(req.body).length === 0) {
+        return res.status(400).json({message: "No data to update"});
+    }
+
+    if (req.body.title !== undefined &&
+        (typeof req.body.title !== "string" ||
+        req.body.title.trim() === "")) {
+        return res.status(400).json({message: "Title must be defined and must be a non-empty string"});
+    }
+    if (req.body.description !== undefined &&
+        typeof req.body.description !== "string") {
+        return res.status(400).json({message: "Description must be a string"});
+    }
+    if (req.body.completed !== undefined &&
+        typeof req.body.completed !== "boolean") {
+        return res.status(400).json({message: "Completed must be a boolean"});
+    }
+
+    // Update the task's properties if they are provided in the request body
+    if (req.body.title !== undefined) {
+        fields.push(`title = $${fields.length + 1}`);
+        values.push(req.body.title.trim());
+    }
+    if (req.body.description !== undefined) {
+        fields.push(`description = $${fields.length + 1}`);
+        values.push(req.body.description.trim());
+    }
+    if (req.body.completed !== undefined) {
+        fields.push(`completed = $${fields.length + 1}`);
+        values.push(req.body.completed);
+    }
+    values.push(id);
+
+    const query = `
+        UPDATE tasks
+        SET ${fields.join(", ")}
+        WHERE id = $${values.length}
+        RETURNING *`;
+    
+    try {
+        const result = await pool.query(query, values);
+
+        if(result.rows.length === 0) {
+            return res.status(404).json({message: "Task not found"});
+        }
+
+        res.status(200).json(result.rows[0]);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: "Error while updating task"});
+    }
+}
+
+async function deleteTask (req, res) {
+    try {
+        const result = await pool.query(`
+            DELETE FROM tasks
+            WHERE id = $1`, [req.params.id]);
+        
+        if (result.rowCount === 0) {
+            return res.status(404).json({message: "Task not found"});
+        }
+        res.status(204).send();
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: "Error while deleting task"});
+    }
+}
+
+module.exports = {
+    getTasks, createTask, updateTask, deleteTask
+};
